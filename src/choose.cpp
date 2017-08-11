2edf4b29a88edc82ff0d7019d11753369c82f0f5
Thomas Strunz <thomas.strunz@avl.com>
2017-08-11 18:02:56 +0200

    Add a CMake build definition file

---
+++ src/choose.cpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2017 Thomas Strunz
+//
+// This file is part of choose.
+//
+// choose is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// choose is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with choose.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <iostream>
+
+bool drawFromBinaryDistribution()
+{
+
+}
+
+int main(int /*argc*/, char **)
+{
+  std::cout << "choose - random path demo                              Thomas Strunz (c) 2017 " << std::endl;
+  
+  return 0;
+}
